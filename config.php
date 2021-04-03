d7470481645cf808c80870098acb176812400062
haranhari11 <69118975+haranhari11@users.noreply.github.com>
2021-04-03 13:18:07 +0530

    Remote connection to sql

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php 
 	
 	// Development Connection
-	$dbhost="localhost";
-	$dbuser="root";
-	$dbpass="";
-	$dbname="login_system";
+// 	$dbhost="localhost";
+// 	$dbuser="root";
+// 	$dbpass="";
+// 	$dbname="login_system";
 
 	// Remote SQL Connection
-	// $dbhost="remotemysql.com";
-	// $dbuser="xFjWG1oTmb";
-	// $dbpass="vS2p3y1UbL";
-	// $dbname="xFjWG1oTmb";
+	$dbhost="remotemysql.com";
+	$dbuser="xFjWG1oTmb";
+	$dbpass="vS2p3y1UbL";
+	$dbname="xFjWG1oTmb";
 
 	$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 	if($conn->connect_error) {
